1cfa8f1df47c445663feb9d117ee851d4b428ca7
bassza009 <bsspawat@gmail.com>
2026-03-24 23:05:22 +0700

    change door go,node 100 to 10000 doors

--- benchmark/door/door.go
+++ benchmark/door/door.go
@@ -9,7 +9,7 @@ func main() {
     var door int = 1
     var incrementer = 0
 	start := time.Now()
-    for current := 1; current <= 100; current++ {
+    for current := 1; current <= 10000; current++ {
         fmt.Printf("Door %d ", current)
 
         if current == door {
--- benchmark/door/door.js
+++ benchmark/door/door.js
@@ -1,6 +1,6 @@
 
 const start = process.hrtime()
-for (var door = 1; door <= 100; door++) {
+for (var door = 1; door <= 10000; door++) {
   var sqrt = Math.sqrt(door);
   if (sqrt === (sqrt | 0)) {
     console.log("Door %d is open", door);
