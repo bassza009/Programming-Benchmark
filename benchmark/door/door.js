aaafb85d9ec5888f10cd2c646827c128711efb3f
bassza009 <bsspawat@gmail.com>
2026-03-25 19:32:17 +0700

    fix bug door js,go

--- benchmark/door/door.js
+++ benchmark/door/door.js
@@ -1,4 +1,3 @@
-cat <<EOF>benchmark/door/door.js
 const start = process.hrtime()
 for (var door = 1; door <= 10000; door++) {
   var sqrt = Math.sqrt(door);
@@ -9,4 +8,3 @@ for (var door = 1; door <= 10000; door++) {
 const end = process.hrtime(start)
 const durationInMs = (end[0] * 1000) + (end[1] / 1000000)
 console.log(`Process duration : ${durationInMs}`)
-EOF
