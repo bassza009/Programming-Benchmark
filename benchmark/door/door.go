c6c4d679c6ec9ed989a745752b3708290559bbb8
bassza009 <bsspawat@gmail.com>
2026-03-25 14:34:20 +0000

    change a little bit

--- benchmark/door/door.go
+++ benchmark/door/door.go
@@ -10,14 +10,14 @@ func main() {
 	var incrementer = 0
 	start := time.Now()
 	for current := 1; current <= 10000; current++ {
-		fmt.Printf("Door %d ", current)
+		//fmt.Printf("Door %d ", current)
 
 		if current == door {
-			fmt.Printf("Open\n")
+			//fmt.Printf("Open\n")
 			incrementer++
 			door += 2*incrementer + 1
 		} else {
-			fmt.Printf("Closed\n")
+			//fmt.Printf("Closed\n")
 		}
 	}
 	duration := time.Since(start)
