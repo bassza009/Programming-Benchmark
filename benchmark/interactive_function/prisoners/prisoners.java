5c2cd019228e34cb27349e9ead64ed8d86902ab7
bassza009 <bsspawat@gmail.com>
2026-04-21 13:20:19 +0000

    change a little bit

--- benchmark/interactive_function/prisoners/prisoners.java
+++ benchmark/interactive_function/prisoners/prisoners.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class prisoners {
     public static void main(String[] args) {
-        int trials = 100000;
+        int trials = 1000000;
         runTrial(trials, "random");
         runTrial(trials, "optimal");
     }
