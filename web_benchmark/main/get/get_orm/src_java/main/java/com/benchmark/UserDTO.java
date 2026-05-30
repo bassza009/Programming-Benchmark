b007a6a349858ce369d35e69d82ed7e0378debdf
bassza009 <bsspawat@gmail.com>
2026-05-30 15:23:07 +0000

    result bme get orm

--- web_benchmark/main/get/get_orm/src_java/main/java/com/benchmark/UserDTO.java
+++ web_benchmark/main/get/get_orm/src_java/main/java/com/benchmark/UserDTO.java
@@ -1,13 +1,5 @@
 package com.benchmark;
 
-public class UserDTO {
-    public Integer id;
-    public String name;
-    public String email;
-
-    public UserDTO(Integer id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
+// 💡 ใช้ Java Record: สั้น กระชับ เร็วปรู๊ดปร๊าด และเป็น Immutable ขนานแท้
+public record UserDTO(Integer id, String name, String email) {
 }
