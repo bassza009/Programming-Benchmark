a366872df68b5173df9ff67a62931a57252d03e5
bassza009 <bsspawat@gmail.com>
2026-06-03 21:26:56 +0700

    fix python post normal

--- web_benchmark/main/post/post_normal/server.py
+++ web_benchmark/main/post/post_normal/server.py
@@ -137,8 +137,8 @@ async def post_3table():
 
 @app.post("/raw/post/4table")
 async def post_4table():
-    try:
-        async with pool.acquire() as conn:
+    async with pool.acquire() as conn:
+        try:
             async with conn.cursor() as cursor:
                 await conn.begin()
 
@@ -162,13 +162,11 @@ async def post_4table():
 
                 await conn.commit()
                 return JSONResponse(status_code=201, content={"user_id": user_id})
-    except Exception as e:
-        try:
+                
+        except Exception as e:
             await conn.rollback()
-        except:
-            pass
-        return JSONResponse(status_code=500, content={"error": str(e)})
-
+            return JSONResponse(status_code=500, content={"error": str(e)})
+        
 @app.on_event("startup")
 async def startup():
     await init_db()
